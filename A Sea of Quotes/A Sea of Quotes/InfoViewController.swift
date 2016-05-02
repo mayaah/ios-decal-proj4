d584be6ef1ea94087d2e5abd5f0f1a8e6e037c6e
Maya Angelica Hernandez <mayaah@berkeley.edu>
2016-05-02 00:19:05 -0700

    added image views to soon-to-be links, I'm done 4 now

--- A Sea of Quotes/A Sea of Quotes/InfoViewController.swift
+++ A Sea of Quotes/A Sea of Quotes/InfoViewController.swift
@@ -7,16 +7,35 @@
 //
 
 import Foundation
+import UIKit
 
 class InfoViewController : UIViewController {
     var nagivationStyleToPresent : String?
+    var transitionOperator = TransitionOperator()
     
-
+    
     @IBOutlet weak var menuItem: UIBarButtonItem!
     @IBOutlet weak var toolbar: UIToolbar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBarHidden = true
+        menuItem.image = UIImage(named: "Menu")
     }
+    
+    @IBAction func presentNavigation(sender: AnyObject?){
+        
+        if self.nagivationStyleToPresent != nil {
+            transitionOperator.transitionStyle = nagivationStyleToPresent!
+            self.performSegueWithIdentifier(nagivationStyleToPresent!, sender: self)
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let toViewController = segue.destinationViewController as UIViewController!
+        self.modalPresentationStyle = UIModalPresentationStyle.Custom
+        toViewController.transitioningDelegate = self.transitionOperator
+    }
+
 }
